706d30b7271da19c8b690945c86e79aa02a5adb7
BillRaymond <BillRaymond@users.noreply.github.com>
2023-01-24 00:48:37 +0000

    Jekyll build from Action

--- wf-01-create-fi/create-featured-image.sh
+++ wf-01-create-fi/create-featured-image.sh
@@ -28,29 +28,3 @@ convert fi-template.png `# load template background image`\
 
 echo “* FINISH Effective, not glamorous agile change”
 
-echo “* START An introduction to Scrum, Part 2: Implementing Scrum and Agility”
-convert fi-template.png `# load template background image`\
-\( ../uploads/wf-guest-images-fi/nader-k-rad.png -resize 282x282 -set page +256+37 \) `# load a guest photo`\
-\( ../uploads/wf-guest-images-fi/seaasons-of-scrum-winter.png -resize 282x282 -set page +256+375 \) `# load a guest photo`\
--layers flatten `# merge the template and guest photos`\
--font 'fonts/ProximaNovaA-Bold.ttf' `# load the font`\
--fill '#f49f1c' -background none `# The podcast title does not have a background`\
--size 580x40 caption:'AGILE IN ACTION PODCAST' `# Add the word PODCAST to the template`\
--geometry +550+46 `# Set the x and y position for the PODCAST text`\
--composite `# Add the text layer to the image`\
--fill white -background none `# The title for the podcast does not have a background`\
--size 580x340 caption:'An introduction to Scrum, Part 2: Implementing Scrum and Agility' `# Podcast title as it appears on the website`\
--geometry +550+96 `# Set the x and y location for the podcast title`\
--composite `# Add the podcast's title to the image`\
--fill white -background none `# the guest names do not have a background`\
--layers flatten `# merge the template and guest photos`\-fill white -background none `# the guest names do not have a background`\
--size 405x195 caption:'with Nader K Rad\nwith Seasons of Scrum' `# List the guest names for the podcast`\
--geometry +550+446 `# Set the x and y position of the guest names`\
--composite `# Add guest names to the image`\
-+write ../uploads/wf-featured-images/2023-01-24-an-introduction-to-scrum-part-2-implementing-scrum-and-agility-no-play.png \
--page +972+448 fi-play-icon.png `# load play icon image`\
--layers flatten \
-../uploads/wf-featured-images/2023-01-24-an-introduction-to-scrum-part-2-implementing-scrum-and-agility.png
-
-echo “* FINISH An introduction to Scrum, Part 2: Implementing Scrum and Agility”
-
