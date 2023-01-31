5906cf41483d6a1c364b05cd75c16f5c38e62753
BillRaymond <BillRaymond@users.noreply.github.com>
2023-01-31 07:35:06 +0000

    Jekyll build from Action

--- wf-01-create-fi/create-featured-image.sh
+++ wf-01-create-fi/create-featured-image.sh
@@ -53,28 +53,3 @@ convert fi-template.png `# load template background image`\
 
 echo “* FINISH The Scrum Master Journey and their Leadership Role”
 
-echo “* START Effective, not glamorous agile change”
-convert fi-template.png `# load template background image`\
-\( ../uploads/wf-guest-images-fi/idzik_img.png -resize 282x282 -set page +256+205 \) `# load a guest photo`\
--layers flatten `# merge the template and guest photos`\
--font 'fonts/ProximaNovaA-Bold.ttf' `# load the font`\
--fill '#f49f1c' -background none `# The podcast title does not have a background`\
--size 580x40 caption:'AGILE IN ACTION PODCAST' `# Add the word PODCAST to the template`\
--geometry +550+46 `# Set the x and y position for the PODCAST text`\
--composite `# Add the text layer to the image`\
--fill white -background none `# The title for the podcast does not have a background`\
--size 580x340 caption:'Effective, not glamorous agile change' `# Podcast title as it appears on the website`\
--geometry +550+96 `# Set the x and y location for the podcast title`\
--composite `# Add the podcast's title to the image`\
--fill white -background none `# the guest names do not have a background`\
--layers flatten `# merge the template and guest photos`\-fill white -background none `# the guest names do not have a background`\
--size 405x195 caption:'with Michal Idzikowski\nAgile Coach at Huuuge Games and leader of the Scrum Master team' `# List the guest names for the podcast`\
--geometry +550+446 `# Set the x and y position of the guest names`\
--composite `# Add guest names to the image`\
-+write ../uploads/wf-featured-images/2023-01-31-effective-not-glamorous-agile-change-no-play.png \
--page +972+448 fi-play-icon.png `# load play icon image`\
--layers flatten \
-../uploads/wf-featured-images/2023-01-31-effective-not-glamorous-agile-change.png
-
-echo “* FINISH Effective, not glamorous agile change”
-
