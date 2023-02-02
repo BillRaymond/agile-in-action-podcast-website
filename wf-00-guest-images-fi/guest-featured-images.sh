a56efff03611e4d1cbf322a74eb829f46007856a
BillRaymond <BillRaymond@users.noreply.github.com>
2023-02-02 02:21:46 +0000

    Jekyll build from Action

--- wf-00-guest-images-fi/guest-featured-images.sh
+++ wf-00-guest-images-fi/guest-featured-images.sh
@@ -2,7 +2,19 @@
 # -e Exit immediately if a command exits with a non-zero status
 # -x Echo all the commands as they run, not just echos
 set -e -x
-echo “* START Converting guest photo: uploads/kathleen-walch.jpeg”
+echo “* START Converting guest photo: uploads/angela-johnson.jpeg”
+convert uploads/angela-johnson.jpeg `# load the guest’s photo`\
+    -resize 250x250^ `# load template background image`\
+    -gravity center `# Set the center of gravity for the photo`\
+    -background transparent `# Background should be transparent`\
+    -extent 250x250 `# Crop the photo to a perfect square`\
+    -bordercolor white `# Define a border for the photo`\
+    -border 6 `# Create a border around the photo`\
+    \( +clone -background black -shadow 60x5+6+6 \) `# Create a shadow under the photo`\
+    +swap -background none -layers merge +repage `# Merge the photo and shadow layers`\
+    uploads/wf-guest-images-fi/angela-johnson.png `# Save the guest photo`\
+
+echo “* FINISH converted guest photo to: uploads/wf-guest-images-fi/angela-johnson.png”echo “* START Converting guest photo: uploads/kathleen-walch.jpeg”
 convert uploads/kathleen-walch.jpeg `# load the guest’s photo`\
     -resize 250x250^ `# load template background image`\
     -gravity center `# Set the center of gravity for the photo`\
--- wf-01-create-fi/create-featured-image.sh
+++ wf-01-create-fi/create-featured-image.sh
@@ -3,6 +3,31 @@
 # -x Echo all the commands as they run, not just echos
 set -e -x
 
+echo “* START Remedies for a Scrum Master with a waterfall hangover”
+convert fi-template.png `# load template background image`\
+\( ../uploads/wf-guest-images-fi/angela-johnson.png -resize 282x282 -set page +256+205 \) `# load a guest photo`\
+-layers flatten `# merge the template and guest photos`\
+-font 'fonts/ProximaNovaA-Bold.ttf' `# load the font`\
+-fill '#f49f1c' -background none `# The podcast title does not have a background`\
+-size 580x40 caption:'AGILE IN ACTION PODCAST' `# Add the word PODCAST to the template`\
+-geometry +550+46 `# Set the x and y position for the PODCAST text`\
+-composite `# Add the text layer to the image`\
+-fill white -background none `# The title for the podcast does not have a background`\
+-size 580x340 caption:'Remedies for a Scrum Master with a waterfall hangover' `# Podcast title as it appears on the website`\
+-geometry +550+96 `# Set the x and y location for the podcast title`\
+-composite `# Add the podcast's title to the image`\
+-fill white -background none `# the guest names do not have a background`\
+-layers flatten `# merge the template and guest photos`\-fill white -background none `# the guest names do not have a background`\
+-size 405x195 caption:'with Angela Johnson\nCertified Scrum Trainer, and Author of the Scrum Master Files' `# List the guest names for the podcast`\
+-geometry +550+446 `# Set the x and y position of the guest names`\
+-composite `# Add guest names to the image`\
++write ../uploads/wf-featured-images/2023-02-28-remedies-for-a-scrum-master-with-a-waterfall-hangover-no-play.png \
+-page +972+448 fi-play-icon.png `# load play icon image`\
+-layers flatten \
+../uploads/wf-featured-images/2023-02-28-remedies-for-a-scrum-master-with-a-waterfall-hangover.png
+
+echo “* FINISH Remedies for a Scrum Master with a waterfall hangover”
+
 echo “* START Introducing strategic AI planning and delivery”
 convert fi-template.png `# load template background image`\
 \( ../uploads/wf-guest-images-fi/kathleen-walch.png -resize 200x200 -set page +295+41 \) `# load a guest photo`\
