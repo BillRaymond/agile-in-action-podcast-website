8456f93ab4ee49c362c1beee7f73a0297c95dfdc
BillRaymond <BillRaymond@users.noreply.github.com>
2023-02-21 07:35:22 +0000

    Jekyll build from Action

--- wf-00-guest-images-fi/guest-featured-images.sh
+++ wf-00-guest-images-fi/guest-featured-images.sh
@@ -182,40 +182,4 @@ convert uploads/build-your-model-for-leading-change.png `# load the guest’s ph
     +swap -background none -layers merge +repage `# Merge the photo and shadow layers`\
     uploads/wf-guest-images-fi/build-your-model-for-leading-change.png `# Save the guest photo`\
 
-echo “* FINISH converted guest photo to: uploads/wf-guest-images-fi/build-your-model-for-leading-change.png”echo “* START Converting guest photo: uploads/kathleen-walch.jpeg”
-convert uploads/kathleen-walch.jpeg `# load the guest’s photo`\
-    -resize 250x250^ `# load template background image`\
-    -gravity center `# Set the center of gravity for the photo`\
-    -background transparent `# Background should be transparent`\
-    -extent 250x250 `# Crop the photo to a perfect square`\
-    -bordercolor white `# Define a border for the photo`\
-    -border 6 `# Create a border around the photo`\
-    \( +clone -background black -shadow 60x5+6+6 \) `# Create a shadow under the photo`\
-    +swap -background none -layers merge +repage `# Merge the photo and shadow layers`\
-    uploads/wf-guest-images-fi/kathleen-walch.png `# Save the guest photo`\
-
-echo “* FINISH converted guest photo to: uploads/wf-guest-images-fi/kathleen-walch.png”echo “* START Converting guest photo: uploads/ronald-schmelzer.jpeg”
-convert uploads/ronald-schmelzer.jpeg `# load the guest’s photo`\
-    -resize 250x250^ `# load template background image`\
-    -gravity center `# Set the center of gravity for the photo`\
-    -background transparent `# Background should be transparent`\
-    -extent 250x250 `# Crop the photo to a perfect square`\
-    -bordercolor white `# Define a border for the photo`\
-    -border 6 `# Create a border around the photo`\
-    \( +clone -background black -shadow 60x5+6+6 \) `# Create a shadow under the photo`\
-    +swap -background none -layers merge +repage `# Merge the photo and shadow layers`\
-    uploads/wf-guest-images-fi/ronald-schmelzer.png `# Save the guest photo`\
-
-echo “* FINISH converted guest photo to: uploads/wf-guest-images-fi/ronald-schmelzer.png”echo “* START Converting guest photo: uploads/new-ai-today-podcast-image.png”
-convert uploads/new-ai-today-podcast-image.png `# load the guest’s photo`\
-    -resize 250x250^ `# load template background image`\
-    -gravity center `# Set the center of gravity for the photo`\
-    -background transparent `# Background should be transparent`\
-    -extent 250x250 `# Crop the photo to a perfect square`\
-    -bordercolor white `# Define a border for the photo`\
-    -border 6 `# Create a border around the photo`\
-    \( +clone -background black -shadow 60x5+6+6 \) `# Create a shadow under the photo`\
-    +swap -background none -layers merge +repage `# Merge the photo and shadow layers`\
-    uploads/wf-guest-images-fi/new-ai-today-podcast-image.png `# Save the guest photo`\
-
-echo “* FINISH converted guest photo to: uploads/wf-guest-images-fi/new-ai-today-podcast-image.png”
+echo “* FINISH converted guest photo to: uploads/wf-guest-images-fi/build-your-model-for-leading-change.png”
