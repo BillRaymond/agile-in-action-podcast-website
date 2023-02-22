d44877a14f9f0ee2b5340313722a491de5883dde
BillRaymond <BillRaymond@users.noreply.github.com>
2023-02-22 01:44:35 +0000

    Jekyll build from Action

--- wf-01-create-fi/create-featured-image.sh
+++ wf-01-create-fi/create-featured-image.sh
@@ -280,29 +280,3 @@ convert fi-template.png `# load template background image`\
 
 echo “* FINISH Remedies for a Scrum Master with a waterfall hangover”
 
-echo “* START Book Launch! Build your Model for Leading Change”
-convert fi-template.png `# load template background image`\
-\( ../uploads/wf-guest-images-fi/marsha-acker.png -resize 282x282 -set page +256+37 \) `# load a guest photo`\
-\( ../uploads/wf-guest-images-fi/build-your-model-for-leading-change.png -resize 282x282 -set page +256+375 \) `# load a guest photo`\
--layers flatten `# merge the template and guest photos`\
--font 'fonts/ProximaNovaA-Bold.ttf' `# load the font`\
--fill '#f49f1c' -background none `# The podcast title does not have a background`\
--size 580x40 caption:'AGILE IN ACTION PODCAST' `# Add the word PODCAST to the template`\
--geometry +550+46 `# Set the x and y position for the PODCAST text`\
--composite `# Add the text layer to the image`\
--fill white -background none `# The title for the podcast does not have a background`\
--size 580x340 caption:'Book Launch! Build your Model for Leading Change' `# Podcast title as it appears on the website`\
--geometry +550+96 `# Set the x and y location for the podcast title`\
--composite `# Add the podcast's title to the image`\
--fill white -background none `# the guest names do not have a background`\
--layers flatten `# merge the template and guest photos`\-fill white -background none `# the guest names do not have a background`\
--size 405x195 caption:'with Marsha Acker\nwith An introduction to Marsha’s latest book, Build Your Model for Leading Change' `# List the guest names for the podcast`\
--geometry +550+446 `# Set the x and y position of the guest names`\
--composite `# Add guest names to the image`\
-+write ../uploads/wf-featured-images/2023-02-22-book-launch-build-your-model-for-leading-change-no-play.png \
--page +972+448 fi-play-icon.png `# load play icon image`\
--layers flatten \
-../uploads/wf-featured-images/2023-02-22-book-launch-build-your-model-for-leading-change.png
-
-echo “* FINISH Book Launch! Build your Model for Leading Change”
-
