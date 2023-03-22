ba484417fedd96392cf40b5ba2829c7860cb8203
BillRaymond <BillRaymond@users.noreply.github.com>
2023-03-22 18:01:47 +0000

    Jekyll build from Action

--- wf-01-create-fi/create-featured-image.sh
+++ wf-01-create-fi/create-featured-image.sh
@@ -3,6 +3,32 @@
 # -x Echo all the commands as they run, not just echos
 set -e -x
 
+echo “* START Digital transformation and agility in government”
+convert fi-template.png `# load template background image`\
+\( ../uploads/wf-guest-images-fi/Maximilian-Kupi.png -resize 282x282 -set page +256+37 \) `# load a guest photo`\
+\( ../uploads/wf-guest-images-fi/Keegan-McBride.png -resize 282x282 -set page +256+375 \) `# load a guest photo`\
+-layers flatten `# merge the template and guest photos`\
+-font 'fonts/ProximaNovaA-Bold.ttf' `# load the font`\
+-fill '#f49f1c' -background none `# The podcast title does not have a background`\
+-size 580x40 caption:'AGILE IN ACTION PODCAST' `# Add the word PODCAST to the template`\
+-geometry +550+46 `# Set the x and y position for the PODCAST text`\
+-composite `# Add the text layer to the image`\
+-fill white -background none `# The title for the podcast does not have a background`\
+-size 580x340 caption:'Digital transformation and agility in government' `# Podcast title as it appears on the website`\
+-geometry +550+96 `# Set the x and y location for the podcast title`\
+-composite `# Add the podcast's title to the image`\
+-fill white -background none `# the guest names do not have a background`\
+-layers flatten `# merge the template and guest photos`\-fill white -background none `# the guest names do not have a background`\
+-size 405x195 caption:'with Maximilian Kupi\nwith Keegan McBride' `# List the guest names for the podcast`\
+-geometry +550+446 `# Set the x and y position of the guest names`\
+-composite `# Add guest names to the image`\
++write ../uploads/wf-featured-images/2023-05-16-digital-transformation-and-agility-in-government-no-play.png \
+-page +972+448 fi-play-icon.png `# load play icon image`\
+-layers flatten \
+../uploads/wf-featured-images/2023-05-16-digital-transformation-and-agility-in-government.png
+
+echo “* FINISH Digital transformation and agility in government”
+
 echo “* START Using metrics to drive value delivery”
 convert fi-template.png `# load template background image`\
 \( ../uploads/wf-guest-images-fi/dave-witkin.png -resize 282x282 -set page +256+205 \) `# load a guest photo`\
@@ -144,7 +170,7 @@ convert fi-template.png `# load template background image`\
 -composite `# Add the podcast's title to the image`\
 -fill white -background none `# the guest names do not have a background`\
 -layers flatten `# merge the template and guest photos`\-fill white -background none `# the guest names do not have a background`\
--size 405x195 caption:'with Phil Simon\nPhil Simon, Probably the worlds leading independent expert on technology and collaboration' `# List the guest names for the podcast`\
+-size 405x195 caption:'with Phil Simon\nPhil Simon, Author, Low-Code/No-Code: Citizen Developers and the Surprising Future of Business Applications (The Future of Work) and collaboration' `# List the guest names for the podcast`\
 -geometry +550+446 `# Set the x and y position of the guest names`\
 -composite `# Add guest names to the image`\
 +write ../uploads/wf-featured-images/2023-04-04-empowering-your-workforce-as-citizen-developers-no-play.png \
