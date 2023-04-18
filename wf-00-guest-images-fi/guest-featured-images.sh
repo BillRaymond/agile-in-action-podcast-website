c3c580e625c253b4997e9e5a7a8614b6a2eb3437
BillRaymond <BillRaymond@users.noreply.github.com>
2023-04-18 07:36:18 +0000

    Jekyll build from Action

--- wf-00-guest-images-fi/guest-featured-images.sh
+++ wf-00-guest-images-fi/guest-featured-images.sh
@@ -200,48 +200,4 @@ convert ../uploads/john-stenbeck.jpeg `# load the guest’s photo`\
     +swap -background none -layers merge +repage `# Merge the photo and shadow layers`\
     ../uploads/wf-guest-images-fi/john-stenbeck.png `# Save the guest photo`\
 
-echo “* FINISH converted guest photo to: ../uploads/wf-guest-images-fi/john-stenbeck.png”echo “photoSource: ../uploads/evan.jpg”
-
-echo “Original photoTarget: ../uploads/evan.jpg”
-
-echo “photoTarget with new url: ../uploads/wf-guest-images-fi/evan.jpg”
-echo “photoTargetExt: jpg”
-echo “photoTargetExtSize: 35”
-echo “photoSource: ../uploads/evan.jpg”
-
-echo “photoTarget: ../uploads/wf-guest-images-fi/evan.png”
-echo “* START Converting guest photo: ../uploads/evan.jpg”
-convert ../uploads/evan.jpg `# load the guest’s photo`\
-    -resize 250x250^ `# load template background image`\
-    -gravity center `# Set the center of gravity for the photo`\
-    -background transparent `# Background should be transparent`\
-    -extent 250x250 `# Crop the photo to a perfect square`\
-    -bordercolor white `# Define a border for the photo`\
-    -border 6 `# Create a border around the photo`\
-    \( +clone -background black -shadow 60x5+6+6 \) `# Create a shadow under the photo`\
-    +swap -background none -layers merge +repage `# Merge the photo and shadow layers`\
-    ../uploads/wf-guest-images-fi/evan.png `# Save the guest photo`\
-
-echo “* FINISH converted guest photo to: ../uploads/wf-guest-images-fi/evan.png”echo “photoSource: ../uploads/martin-foster.jpeg”
-
-echo “Original photoTarget: ../uploads/martin-foster.jpeg”
-
-echo “photoTarget with new url: ../uploads/wf-guest-images-fi/martin-foster.jpeg”
-echo “photoTargetExt: jpeg”
-echo “photoTargetExtSize: 44”
-echo “photoSource: ../uploads/martin-foster.jpeg”
-
-echo “photoTarget: ../uploads/wf-guest-images-fi/martin-foster.png”
-echo “* START Converting guest photo: ../uploads/martin-foster.jpeg”
-convert ../uploads/martin-foster.jpeg `# load the guest’s photo`\
-    -resize 250x250^ `# load template background image`\
-    -gravity center `# Set the center of gravity for the photo`\
-    -background transparent `# Background should be transparent`\
-    -extent 250x250 `# Crop the photo to a perfect square`\
-    -bordercolor white `# Define a border for the photo`\
-    -border 6 `# Create a border around the photo`\
-    \( +clone -background black -shadow 60x5+6+6 \) `# Create a shadow under the photo`\
-    +swap -background none -layers merge +repage `# Merge the photo and shadow layers`\
-    ../uploads/wf-guest-images-fi/martin-foster.png `# Save the guest photo`\
-
-echo “* FINISH converted guest photo to: ../uploads/wf-guest-images-fi/martin-foster.png”
+echo “* FINISH converted guest photo to: ../uploads/wf-guest-images-fi/john-stenbeck.png”
