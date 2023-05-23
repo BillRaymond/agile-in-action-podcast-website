e3067c13b6bcb8cbcc64fe84d8b3b79c92f518d7
BillRaymond <BillRaymond@users.noreply.github.com>
2023-05-23 07:34:25 +0000

    Jekyll build from Action

--- wf-01-create-fi/create-featured-image.sh
+++ wf-01-create-fi/create-featured-image.sh
@@ -78,28 +78,3 @@ convert fi-template.png `# load template background image`\
 
 echo “* FINISH Align with yourself, align with your teams”
 
-echo “* START AI Transformation and Agile Leadership”
-convert fi-template.png `# load template background image`\
-\( ../uploads/wf-guest-images-fi/zorina-alliata.png -resize 282x282 -set page +256+205 \) `# load a guest photo`\
--layers flatten `# merge the template and guest photos`\
--font 'fonts/ProximaNovaA-Bold.ttf' `# load the font`\
--fill '#f49f1c' -background none `# The podcast title does not have a background`\
--size 580x40 caption:'AGILE IN ACTION PODCAST' `# Add the word PODCAST to the template`\
--geometry +550+46 `# Set the x and y position for the PODCAST text`\
--composite `# Add the text layer to the image`\
--fill white -background none `# The title for the podcast does not have a background`\
--size 580x340 caption:'AI Transformation and Agile Leadership' `# Podcast title as it appears on the website`\
--geometry +550+96 `# Set the x and y location for the podcast title`\
--composite `# Add the podcast's title to the image`\
--fill white -background none `# the guest names do not have a background`\
--layers flatten `# merge the template and guest photos`\-fill white -background none `# the guest names do not have a background`\
--size 405x195 caption:'with Zorina Alliata\nSr. Global Machine Learning Strategist, Amazon, Chair AI Committee at AnitaB.org, Georgetown University Adjunct Faculty' `# List the guest names for the podcast`\
--geometry +550+446 `# Set the x and y position of the guest names`\
--composite `# Add guest names to the image`\
-+write ../uploads/wf-featured-images/2023-05-23-ai-transformation-and-agile-leadership-no-play.png \
--page +972+448 fi-play-icon.png `# load play icon image`\
--layers flatten \
-../uploads/wf-featured-images/2023-05-23-ai-transformation-and-agile-leadership.png
-
-echo “* FINISH AI Transformation and Agile Leadership”
-
