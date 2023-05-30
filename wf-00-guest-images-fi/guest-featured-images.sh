d686c46164a77e0a6c709ea0e58575c9287aa704
BillRaymond <BillRaymond@users.noreply.github.com>
2023-05-30 07:34:16 +0000

    Jekyll build from Action

--- wf-00-guest-images-fi/guest-featured-images.sh
+++ wf-00-guest-images-fi/guest-featured-images.sh
@@ -46,26 +46,4 @@ convert ../uploads/marya-triandafellos.jpeg `# load the guest’s photo`\
     +swap -background none -layers merge +repage `# Merge the photo and shadow layers`\
     ../uploads/wf-guest-images-fi/marya-triandafellos.png `# Save the guest photo`\
 
-echo “* FINISH converted guest photo to: ../uploads/wf-guest-images-fi/marya-triandafellos.png”echo “photoSource: ../uploads/peter-stevens.jpeg”
-
-echo “Original photoTarget: ../uploads/peter-stevens.jpeg”
-
-echo “photoTarget with new url: ../uploads/wf-guest-images-fi/peter-stevens.jpeg”
-echo “photoTargetExt: jpeg”
-echo “photoTargetExtSize: 44”
-echo “photoSource: ../uploads/peter-stevens.jpeg”
-
-echo “photoTarget: ../uploads/wf-guest-images-fi/peter-stevens.png”
-echo “* START Converting guest photo: ../uploads/peter-stevens.jpeg”
-convert ../uploads/peter-stevens.jpeg `# load the guest’s photo`\
-    -resize 250x250^ `# load template background image`\
-    -gravity center `# Set the center of gravity for the photo`\
-    -background transparent `# Background should be transparent`\
-    -extent 250x250 `# Crop the photo to a perfect square`\
-    -bordercolor white `# Define a border for the photo`\
-    -border 6 `# Create a border around the photo`\
-    \( +clone -background black -shadow 60x5+6+6 \) `# Create a shadow under the photo`\
-    +swap -background none -layers merge +repage `# Merge the photo and shadow layers`\
-    ../uploads/wf-guest-images-fi/peter-stevens.png `# Save the guest photo`\
-
-echo “* FINISH converted guest photo to: ../uploads/wf-guest-images-fi/peter-stevens.png”
+echo “* FINISH converted guest photo to: ../uploads/wf-guest-images-fi/marya-triandafellos.png”
--- wf-01-create-fi/create-featured-image.sh
+++ wf-01-create-fi/create-featured-image.sh
@@ -53,28 +53,3 @@ convert fi-template.png `# load template background image`\
 
 echo “* FINISH Introducing DesignOps”
 
-echo “* START Align with yourself, align with your teams”
-convert fi-template.png `# load template background image`\
-\( ../uploads/wf-guest-images-fi/peter-stevens.png -resize 282x282 -set page +256+205 \) `# load a guest photo`\
--layers flatten `# merge the template and guest photos`\
--font 'fonts/ProximaNovaA-Bold.ttf' `# load the font`\
--fill '#f49f1c' -background none `# The podcast title does not have a background`\
--size 580x40 caption:'AGILE IN ACTION PODCAST' `# Add the word PODCAST to the template`\
--geometry +550+46 `# Set the x and y position for the PODCAST text`\
--composite `# Add the text layer to the image`\
--fill white -background none `# The title for the podcast does not have a background`\
--size 580x340 caption:'Align with yourself, align with your teams' `# Podcast title as it appears on the website`\
--geometry +550+96 `# Set the x and y location for the podcast title`\
--composite `# Add the podcast's title to the image`\
--fill white -background none `# the guest names do not have a background`\
--layers flatten `# merge the template and guest photos`\-fill white -background none `# the guest names do not have a background`\
--size 405x195 caption:'with Peter B. Stevens\nCreator of The Personal Agility System, Chief Agility Officer,  Author,  and Keynote Speaker' `# List the guest names for the podcast`\
--geometry +550+446 `# Set the x and y position of the guest names`\
--composite `# Add guest names to the image`\
-+write ../uploads/wf-featured-images/2023-05-30-align-with-yourself-align-with-your-teams-no-play.png \
--page +972+448 fi-play-icon.png `# load play icon image`\
--layers flatten \
-../uploads/wf-featured-images/2023-05-30-align-with-yourself-align-with-your-teams.png
-
-echo “* FINISH Align with yourself, align with your teams”
-
