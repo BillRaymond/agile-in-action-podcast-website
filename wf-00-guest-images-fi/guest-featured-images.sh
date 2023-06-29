7fb63a774518da85510908eb69065f7f53c68eac
BillRaymond <BillRaymond@users.noreply.github.com>
2023-06-29 00:41:32 +0000

    Jekyll build from Action

--- wf-00-guest-images-fi/guest-featured-images.sh
+++ wf-00-guest-images-fi/guest-featured-images.sh
@@ -2,7 +2,29 @@
 # -e Exit immediately if a command exits with a non-zero status
 # -x Echo all the commands as they run, not just echos
 set -e -x
-echo “photoSource: ../uploads/jay-palat.jpeg”
+echo “photoSource: ../uploads/jan-otterbach.jpeg”
+
+echo “Original photoTarget: ../uploads/jan-otterbach.jpeg”
+
+echo “photoTarget with new url: ../uploads/wf-guest-images-fi/jan-otterbach.jpeg”
+echo “photoTargetExt: jpeg”
+echo “photoTargetExtSize: 44”
+echo “photoSource: ../uploads/jan-otterbach.jpeg”
+
+echo “photoTarget: ../uploads/wf-guest-images-fi/jan-otterbach.png”
+echo “* START Converting guest photo: ../uploads/jan-otterbach.jpeg”
+convert ../uploads/jan-otterbach.jpeg `# load the guest’s photo`\
+    -resize 250x250^ `# load template background image`\
+    -gravity center `# Set the center of gravity for the photo`\
+    -background transparent `# Background should be transparent`\
+    -extent 250x250 `# Crop the photo to a perfect square`\
+    -bordercolor white `# Define a border for the photo`\
+    -border 6 `# Create a border around the photo`\
+    \( +clone -background black -shadow 60x5+6+6 \) `# Create a shadow under the photo`\
+    +swap -background none -layers merge +repage `# Merge the photo and shadow layers`\
+    ../uploads/wf-guest-images-fi/jan-otterbach.png `# Save the guest photo`\
+
+echo “* FINISH converted guest photo to: ../uploads/wf-guest-images-fi/jan-otterbach.png”echo “photoSource: ../uploads/jay-palat.jpeg”
 
 echo “Original photoTarget: ../uploads/jay-palat.jpeg”
 
--- wf-01-create-fi/create-featured-image.sh
+++ wf-01-create-fi/create-featured-image.sh
@@ -3,6 +3,31 @@
 # -x Echo all the commands as they run, not just echos
 set -e -x
 
+echo “* START Behavorial change is the heart of agile”
+convert fi-template.png `# load template background image`\
+\( ../uploads/wf-guest-images-fi/jan-otterbach.png -resize 282x282 -set page +256+205 \) `# load a guest photo`\
+-layers flatten `# merge the template and guest photos`\
+-font 'fonts/ProximaNovaA-Bold.ttf' `# load the font`\
+-fill '#f49f1c' -background none `# The podcast title does not have a background`\
+-size 580x40 caption:'AGILE IN ACTION PODCAST' `# Add the word PODCAST to the template`\
+-geometry +550+46 `# Set the x and y position for the PODCAST text`\
+-composite `# Add the text layer to the image`\
+-fill white -background none `# The title for the podcast does not have a background`\
+-size 580x340 caption:'Behavorial change is the heart of agile' `# Podcast title as it appears on the website`\
+-geometry +550+96 `# Set the x and y location for the podcast title`\
+-composite `# Add the podcast's title to the image`\
+-fill white -background none `# the guest names do not have a background`\
+-layers flatten `# merge the template and guest photos`\-fill white -background none `# the guest names do not have a background`\
+-size 405x195 caption:'with Jan Otterbach\nVice President and Business Agility Lead at Elavon-Europe US Bank' `# List the guest names for the podcast`\
+-geometry +550+446 `# Set the x and y position of the guest names`\
+-composite `# Add guest names to the image`\
++write ../uploads/wf-featured-images/2023-07-25-behavorial-change-is-the-heart-of-agile-no-play.png \
+-page +972+448 fi-play-icon.png `# load play icon image`\
+-layers flatten \
+../uploads/wf-featured-images/2023-07-25-behavorial-change-is-the-heart-of-agile.png
+
+echo “* FINISH Behavorial change is the heart of agile”
+
 echo “* START To AI or not to AI”
 convert fi-template.png `# load template background image`\
 \( ../uploads/wf-guest-images-fi/jay-palat.png -resize 282x282 -set page +256+205 \) `# load a guest photo`\
