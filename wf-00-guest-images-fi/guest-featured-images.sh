86fa604a07dbb34d1192d789f8bca6e97ba8c05e
BillRaymond <BillRaymond@users.noreply.github.com>
2023-08-01 07:34:32 +0000

    Jekyll build from Action

--- wf-00-guest-images-fi/guest-featured-images.sh
+++ wf-00-guest-images-fi/guest-featured-images.sh
@@ -24,26 +24,4 @@ convert ../uploads/Jonathon-Hensley.jpeg `# load the guest’s photo`\
     +swap -background none -layers merge +repage `# Merge the photo and shadow layers`\
     ../uploads/wf-guest-images-fi/Jonathon-Hensley.png `# Save the guest photo`\
 
-echo “* FINISH converted guest photo to: ../uploads/wf-guest-images-fi/Jonathon-Hensley.png”echo “photoSource: ../uploads/jan-otterbach-2.jpeg”
-
-echo “Original photoTarget: ../uploads/jan-otterbach-2.jpeg”
-
-echo “photoTarget with new url: ../uploads/wf-guest-images-fi/jan-otterbach-2.jpeg”
-echo “photoTargetExt: jpeg”
-echo “photoTargetExtSize: 46”
-echo “photoSource: ../uploads/jan-otterbach-2.jpeg”
-
-echo “photoTarget: ../uploads/wf-guest-images-fi/jan-otterbach-2.png”
-echo “* START Converting guest photo: ../uploads/jan-otterbach-2.jpeg”
-convert ../uploads/jan-otterbach-2.jpeg `# load the guest’s photo`\
-    -resize 250x250^ `# load template background image`\
-    -gravity center `# Set the center of gravity for the photo`\
-    -background transparent `# Background should be transparent`\
-    -extent 250x250 `# Crop the photo to a perfect square`\
-    -bordercolor white `# Define a border for the photo`\
-    -border 6 `# Create a border around the photo`\
-    \( +clone -background black -shadow 60x5+6+6 \) `# Create a shadow under the photo`\
-    +swap -background none -layers merge +repage `# Merge the photo and shadow layers`\
-    ../uploads/wf-guest-images-fi/jan-otterbach-2.png `# Save the guest photo`\
-
-echo “* FINISH converted guest photo to: ../uploads/wf-guest-images-fi/jan-otterbach-2.png”
+echo “* FINISH converted guest photo to: ../uploads/wf-guest-images-fi/Jonathon-Hensley.png”
