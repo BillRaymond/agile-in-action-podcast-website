ff413ce27ab5e76a1b178586bfb80f6d07d1bfbe
BillRaymond <BillRaymond@users.noreply.github.com>
2023-08-08 07:34:03 +0000

    Jekyll build from Action

--- wf-01-create-fi/create-featured-image.sh
+++ wf-01-create-fi/create-featured-image.sh
@@ -2,29 +2,3 @@
 # -e Exit immediately if a command exits with a non-zero status
 # -x Echo all the commands as they run, not just echos
 set -e -x
-
-echo “* START Amplify your impact: Unleash the power of alignment to deliver value”
-convert fi-template.png `# load template background image`\
-\( ../uploads/wf-guest-images-fi/Jonathon-Hensley.png -resize 282x282 -set page +256+205 \) `# load a guest photo`\
--layers flatten `# merge the template and guest photos`\
--font 'fonts/ProximaNovaA-Bold.ttf' `# load the font`\
--fill '#f49f1c' -background none `# The podcast title does not have a background`\
--size 580x40 caption:'AGILE IN ACTION PODCAST' `# Add the word PODCAST to the template`\
--geometry +550+46 `# Set the x and y position for the PODCAST text`\
--composite `# Add the text layer to the image`\
--fill white -background none `# The title for the podcast does not have a background`\
--size 580x340 caption:'Amplify your impact: Unleash the power of alignment to deliver value' `# Podcast title as it appears on the website`\
--geometry +550+96 `# Set the x and y location for the podcast title`\
--composite `# Add the podcast's title to the image`\
--fill white -background none `# the guest names do not have a background`\
--layers flatten `# merge the template and guest photos`\-fill white -background none `# the guest names do not have a background`\
--size 405x195 caption:'with Jonathon Hensley\nCEO of EMERGE and Author of Alignment: Overcoming  internal sabotage and digital product failure' `# List the guest names for the podcast`\
--geometry +550+446 `# Set the x and y position of the guest names`\
--composite `# Add guest names to the image`\
-+write ../uploads/wf-featured-images/2023-08-08-amplify-your-impact-unleash-the-power-of-alignment-to-deliver-value-no-play.png \
--page +972+448 fi-play-icon.png `# load play icon image`\
--layers flatten \
-../uploads/wf-featured-images/2023-08-08-amplify-your-impact-unleash-the-power-of-alignment-to-deliver-value.png
-
-echo “* FINISH Amplify your impact: Unleash the power of alignment to deliver value”
-
