b17f42168a2e4381378b6ed77fba519ed2a05aa7
BillRaymond <BillRaymond@users.noreply.github.com>
2023-10-03 00:03:10 +0000

    Jekyll build from Action

--- wf-00-guest-images-fi/guest-featured-images.sh
+++ wf-00-guest-images-fi/guest-featured-images.sh
@@ -2,7 +2,29 @@
 # -e Exit immediately if a command exits with a non-zero status
 # -x Echo all the commands as they run, not just echos
 set -e -x
-echo “photoSource: ../uploads/pamela-meyer.jpeg”
+echo “photoSource: ../uploads/chuck-mollor.jpeg”
+
+echo “Original photoTarget: ../uploads/chuck-mollor.jpeg”
+
+echo “photoTarget with new url: ../uploads/wf-guest-images-fi/chuck-mollor.jpeg”
+echo “photoTargetExt: jpeg”
+echo “photoTargetExtSize: 43”
+echo “photoSource: ../uploads/chuck-mollor.jpeg”
+
+echo “photoTarget: ../uploads/wf-guest-images-fi/chuck-mollor.png”
+echo “* START Converting guest photo: ../uploads/chuck-mollor.jpeg”
+convert ../uploads/chuck-mollor.jpeg `# load the guest’s photo`\
+    -resize 250x250^ `# load template background image`\
+    -gravity center `# Set the center of gravity for the photo`\
+    -background transparent `# Background should be transparent`\
+    -extent 250x250 `# Crop the photo to a perfect square`\
+    -bordercolor white `# Define a border for the photo`\
+    -border 6 `# Create a border around the photo`\
+    \( +clone -background black -shadow 60x5+6+6 \) `# Create a shadow under the photo`\
+    +swap -background none -layers merge +repage `# Merge the photo and shadow layers`\
+    ../uploads/wf-guest-images-fi/chuck-mollor.png `# Save the guest photo`\
+
+echo “* FINISH converted guest photo to: ../uploads/wf-guest-images-fi/chuck-mollor.png”echo “photoSource: ../uploads/pamela-meyer.jpeg”
 
 echo “Original photoTarget: ../uploads/pamela-meyer.jpeg”
 
@@ -24,26 +46,4 @@ convert ../uploads/pamela-meyer.jpeg `# load the guest’s photo`\
     +swap -background none -layers merge +repage `# Merge the photo and shadow layers`\
     ../uploads/wf-guest-images-fi/pamela-meyer.png `# Save the guest photo`\
 
-echo “* FINISH converted guest photo to: ../uploads/wf-guest-images-fi/pamela-meyer.png”echo “photoSource: ../uploads/pia-wendelbo-v1.png”
-
-echo “Original photoTarget: ../uploads/pia-wendelbo-v1.png”
-
-echo “photoTarget with new url: ../uploads/wf-guest-images-fi/pia-wendelbo-v1.png”
-echo “photoTargetExt: png”
-echo “photoTargetExtSize: 46”
-echo “photoSource: ../uploads/pia-wendelbo-v1.png”
-
-echo “photoTarget: ../uploads/wf-guest-images-fi/pia-wendelbo-v1.png”
-echo “* START Converting guest photo: ../uploads/pia-wendelbo-v1.png”
-convert ../uploads/pia-wendelbo-v1.png `# load the guest’s photo`\
-    -resize 250x250^ `# load template background image`\
-    -gravity center `# Set the center of gravity for the photo`\
-    -background transparent `# Background should be transparent`\
-    -extent 250x250 `# Crop the photo to a perfect square`\
-    -bordercolor white `# Define a border for the photo`\
-    -border 6 `# Create a border around the photo`\
-    \( +clone -background black -shadow 60x5+6+6 \) `# Create a shadow under the photo`\
-    +swap -background none -layers merge +repage `# Merge the photo and shadow layers`\
-    ../uploads/wf-guest-images-fi/pia-wendelbo-v1.png `# Save the guest photo`\
-
-echo “* FINISH converted guest photo to: ../uploads/wf-guest-images-fi/pia-wendelbo-v1.png”
+echo “* FINISH converted guest photo to: ../uploads/wf-guest-images-fi/pamela-meyer.png”
