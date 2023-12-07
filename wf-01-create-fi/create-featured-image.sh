da05a0502f021946ef8994a6593ac65d61a040ac
BillRaymond <BillRaymond@users.noreply.github.com>
2023-12-07 03:02:20 +0000

    Jekyll build from Action

--- wf-01-create-fi/create-featured-image.sh
+++ wf-01-create-fi/create-featured-image.sh
@@ -3,7 +3,7 @@
 # -x Echo all the commands as they run, not just echos
 set -e -x
 
-echo “* START Becoming a Great Manger”
+echo “* START Becoming a Great Manager”
 convert fi-template.png `# load template background image`\
 \( ../uploads/wf-guest-images-fi/jack-skeels.png -resize 282x282 -set page +256+205 \) `# load a guest photo`\
 -layers flatten `# merge the template and guest photos`\
@@ -13,7 +13,7 @@ convert fi-template.png `# load template background image`\
 -geometry +550+46 `# Set the x and y position for the PODCAST text`\
 -composite `# Add the text layer to the image`\
 -fill white -background none `# The title for the podcast does not have a background`\
--size 580x340 caption:'Becoming a Great Manger' `# Podcast title as it appears on the website`\
+-size 580x340 caption:'Becoming a Great Manager' `# Podcast title as it appears on the website`\
 -geometry +550+96 `# Set the x and y location for the podcast title`\
 -composite `# Add the podcast's title to the image`\
 -fill white -background none `# the guest names do not have a background`\
@@ -21,10 +21,10 @@ convert fi-template.png `# load template background image`\
 -size 405x195 caption:'with Jack Skeels\nAuthor of UNMANAGED, CEO of AgencyAgile' `# List the guest names for the podcast`\
 -geometry +550+446 `# Set the x and y position of the guest names`\
 -composite `# Add guest names to the image`\
-+write ../uploads/wf-featured-images/2023-12-12-becoming-a-great-manger-no-play.png \
++write ../uploads/wf-featured-images/2023-12-12-becoming-a-great-manager-no-play.png \
 -page +972+448 fi-play-icon-youtube.png `# load YouTube icon image`\
 -layers flatten \
-../uploads/wf-featured-images/2023-12-12-becoming-a-great-manger.png
+../uploads/wf-featured-images/2023-12-12-becoming-a-great-manager.png
 
-echo “* FINISH Becoming a Great Manger”
+echo “* FINISH Becoming a Great Manager”
 
