235c82f85209a1f3f61b0cd04c8e9d725dd83731
BillRaymond <BillRaymond@users.noreply.github.com>
2025-05-19 13:42:21 -0700

    Jekyll build from Action

--- wf-00-guest-images-fi/guest-featured-images.sh
+++ wf-00-guest-images-fi/guest-featured-images.sh
@@ -2,3 +2,26 @@
 # -e Exit immediately if a command exits with a non-zero status
 # -x Echo all the commands as they run, not just echos
 set -e -x
+echo “photoSource: ../uploads/adam-boddison-headshot.png”
+
+echo “Original photoTarget: ../uploads/adam-boddison-headshot.png”
+
+echo “photoTarget with new url: ../uploads/wf-guest-images-fi/adam-boddison-headshot.png”
+echo “photoTargetExt: png”
+echo “photoTargetExtSize: 53”
+echo “photoSource: ../uploads/adam-boddison-headshot.png”
+
+echo “photoTarget: ../uploads/wf-guest-images-fi/adam-boddison-headshot.png”
+echo “* START Converting guest photo: ../uploads/adam-boddison-headshot.png”
+convert ../uploads/adam-boddison-headshot.png `# load the guest’s photo`\
+    -resize 250x250^ `# load template background image`\
+    -gravity center `# Set the center of gravity for the photo`\
+    -background transparent `# Background should be transparent`\
+    -extent 250x250 `# Crop the photo to a perfect square`\
+    -bordercolor white `# Define a border for the photo`\
+    -border 6 `# Create a border around the photo`\
+    \( +clone -background black -shadow 60x5+6+6 \) `# Create a shadow under the photo`\
+    +swap -background none -layers merge +repage `# Merge the photo and shadow layers`\
+    ../uploads/wf-guest-images-fi/adam-boddison-headshot.png `# Save the guest photo`\
+
+echo “* FINISH converted guest photo to: ../uploads/wf-guest-images-fi/adam-boddison-headshot.png”
